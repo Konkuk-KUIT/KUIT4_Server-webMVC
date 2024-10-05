8d83ae3097e0c56c6cd8d89f340024c5a8d82901
JoGyuBin <rbqls3529@gmail.com>
2024-10-05 15:22:11 +0900

    요구사항 4완료

---
+++ src/main/java/controller/LoginUserController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet ("/user/login")
+public class LoginUserController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        MemoryUserRepository memoryUserRepository = MemoryUserRepository.getInstance();
+        User user = memoryUserRepository.findUserById(req.getParameter("userId"));
+
+        if (user != null && user.getPassword().equals(req.getParameter("password"))) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            resp.sendRedirect("/index.jsp");
+        } else {
+            resp.sendRedirect("/user/login_failed.jsp");
+        }
+    }
+}
