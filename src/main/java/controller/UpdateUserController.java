cb80fc6bdca090c773248bf6c828b2ac81338844
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2024-10-07 17:01:41 +0900

    feat : 미션 0단계 요구사항 5 구현

--- src/main/java/controller/UpdateUserController.java
+++ src/main/java/controller/UpdateUserController.java
@@ -8,12 +8,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/update")
 public class UpdateUserController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         User user = new User(req.getParameter("userId"),
                 req.getParameter("password"),
                 req.getParameter("name"),
--- src/main/java/controller/UpdateUserFormController.java
+++ src/main/java/controller/UpdateUserFormController.java
@@ -8,15 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
 public class UpdateUserFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        User loginedUser = (User) value;
         String userId = req.getParameter("userId");
         User user = MemoryUserRepository.getInstance().findUserById(userId);
-        req.setAttribute("users", user);
-        req.getRequestDispatcher("/user/updateForm.jsp").forward(req, resp);
+        if(loginedUser.equals(user)) {
+                req.setAttribute("users", user);
+                req.getRequestDispatcher("/user/updateForm.jsp").forward(req, resp);
+                return;
+        }
+        resp.sendRedirect("/user/userList");
+
+
     }
 }
