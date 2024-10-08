9d6916e9992ef29d61d74c47e3fa340e198df01c
mr8356 <mr8356@naver.com>
2024-10-09 01:17:37 +0900

    complete requirement 2

--- src/main/java/core/DispatcherServlet.java
+++ src/main/java/core/DispatcherServlet.java
@@ -14,42 +14,22 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
     private static final RequestMapper requestMapper = new RequestMapper();
 
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String url = request.getRequestURI();
-        Controller controller = requestMapper.getController(url);
-        // 정적 자원 요청을 필터링
-        if (url.endsWith(".html") || url.endsWith(".css") || url.endsWith(".js")) {
-            // 정적 자원 파일을 직접 반환
-            request.getRequestDispatcher(url).forward(request, response);
-            return; // 메서드를 종료하여 이후 로직 실행 방지
-        }
-
-        if (controller != null) {
-            try {
-                String view = controller.handleRequest(request, response);
-                if (view.startsWith("redirect:")) {
-                    response.sendRedirect(view.substring("redirect:".length())); // "redirect:" 제거
-
-                } else {
-                    request.getRequestDispatcher(view).forward(request, response); // forward
-                }
-            } catch (IOException e) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "서버 내부 오류 발생"); // 500 에러
-            }
-        } else {
-            response.sendError(StatusCode.NOT_FOUND.getCode()); // 404 Not Found
-        }
+        process(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    private static void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String url = request.getRequestURI();
         Controller controller = requestMapper.getController(url);
         // 정적 자원 요청을 필터링
         if (url.endsWith(".html") || url.endsWith(".css") || url.endsWith(".js")) {
             // 정적 자원 파일을 직접 반환
             request.getRequestDispatcher(url).forward(request, response);
-            return; // 메서드를 종료하여 이후 로직 실행 방지
+            return;
         }
 
         if (controller != null) {
@@ -68,4 +48,5 @@ public class DispatcherServlet extends HttpServlet {
             response.sendError(StatusCode.NOT_FOUND.getCode()); // 404 Not Found
         }
     }
+
 }
