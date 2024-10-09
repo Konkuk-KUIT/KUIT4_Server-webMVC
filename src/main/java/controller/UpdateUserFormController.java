034756a1cbfb9786fb67e1f461652e2334d4492d
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-10-10 03:15:17 +0900

    feat: 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/controller/UpdateUserFormController.java
+++ src/main/java/controller/UpdateUserFormController.java
@@ -8,16 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
 public class UpdateUserFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        User loggedUser = (User) value;
+
         String userId = req.getParameter("userId");
         User user = MemoryUserRepository.getInstance().findUserById(userId);
 
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("/user/updateForm.jsp").forward(req, resp);
+        if(loggedUser.equals(user)) {
+            req.setAttribute("users", user);
+            req.getRequestDispatcher("/user/updateForm.jsp").forward(req, resp);
+            return;
+        }
+        resp.sendRedirect("/user/userList");
     }
 }
