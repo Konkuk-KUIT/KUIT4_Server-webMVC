313d500dbb6d9ce75e6dc4b5d1d31e713079403e
cyLee1111 <flora021111@gmail.com>
2024-10-09 17:20:40 +0900

    feat: add Level 1-4: login/logout

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = MemoryUserRepository.getInstance().findUserById(userId);
+        // 세션 정보 저장
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
+        System.out.println("로그인");
+        resp.sendRedirect("/");
+    }
+}
