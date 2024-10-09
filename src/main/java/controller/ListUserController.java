c16f965ba2b50c16c8a6fa9a935fb832df580833
yskim6772 <yskim6772@gmail.com>
2024-10-09 16:23:26 +0900

    [feat] : step 1 - requirement 5

--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -9,13 +9,26 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Collection;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
+
+    private static final String USER_SESSION_KEY = "user"; // 세션에 저장된 사용자 정보의 키
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute(USER_SESSION_KEY);
+
+        // 로그인 되어 있지 않으면 로그인 페이지로 redirect
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
         Collection<User> users = MemoryUserRepository.getInstance().findAll();
         req.setAttribute("users", users);
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
