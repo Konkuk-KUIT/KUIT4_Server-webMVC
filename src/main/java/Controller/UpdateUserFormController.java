7f240088f1dd84abc8680f5df6d20601835767fc
minjin0610 <138571857+minjin0610@users.noreply.github.com>
2024-10-10 22:27:08 +0900

    webMVC complete

---
+++ src/main/java/Controller/UpdateUserFormController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UpdateUserFormController implements Controller {
+
+    @Override
+    public String handleRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 사용자 정보를 받아서 JSP로 전달
+        User userById = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
+        req.setAttribute("user", userById);
+        return "/user/updateForm";
+    }
+}
