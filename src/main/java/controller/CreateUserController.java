3060a7274fa463b2054752d0f25cddb459cb9bdb
seungwoo1209 <2002msw@naver.com>
2024-10-10 21:22:47 +0900

    step 1 reqirement 4 complete

--- src/main/java/controller/CreateUserController.java
+++ src/main/java/controller/CreateUserController.java
@@ -22,6 +22,7 @@ public class CreateUserController extends HttpServlet {
 
         MemoryUserRepository.getInstance().addUser(user);
         System.out.println("user 회원가입 완료");
+
         resp.sendRedirect("/user/list");
     }
 }
---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User userFromRequest = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
+        if(!isIdExist(userFromRequest)) {
+            System.err.println("아이디의 계정이 존재하지 않음.");
+            resp.sendRedirect("/user/login_failed.jsp");
+            return;
+        }
+
+        if(!isPasswordMatchUser(req, userFromRequest)) {
+            System.err.println("비밀번호가 일치하지 않음.");
+            resp.sendRedirect("/user/login_failed.jsp");
+            return;
+        }
+
+        // 로그인 성공
+        HttpSession session = req.getSession();
+        session.setAttribute("user", userFromRequest);
+        resp.sendRedirect("/");
+        System.out.println("로그인 성공");
+
+    }
+
+    private boolean isIdExist(User userFromRequest) {
+        return userFromRequest != null;
+    }
+
+    private boolean isPasswordMatchUser(HttpServletRequest req, User userFromRequest) {
+        return userFromRequest.getPassword().equals(req.getParameter("password"));
+    }
+}
