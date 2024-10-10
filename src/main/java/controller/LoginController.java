4675ab7f1596e6cb6a3f5b076835b34d673ffdaa
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-10-10 20:16:11 +0900

    fix: 스터디 후 문제 해결

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -23,6 +23,7 @@ public class LoginController extends HttpServlet {
         User user = MemoryUserRepository.getInstance().findUserById(userId);
 
         // 로그인 실패 시 sendRedirect를 사용하여 실패 페이지로 이동시킴
+        // 새로운 HTTP 요청이 발생하기 때문에 요청/응답 사이클이 새로 시작됨
         if(user == null || !user.getPassword().equals(password)) {
             resp.sendRedirect( "/user/login_failed.jsp");
             System.out.println("user 로그인 실패");
@@ -30,6 +31,7 @@ public class LoginController extends HttpServlet {
         }
 
         // 로그인 성공 시 forward를 사용하여 로그인 성공 후 데이터를 유지하며 페이지를 렌더링 시킴
+        // 기존 요청과 응답을 그대로 전달하기 때문에 같은 요청 객체와 데이터를 사용할 수 있음
         System.out.println("user 로그인 성공");
         HttpSession session = req.getSession();
         session.setAttribute("user", user);
