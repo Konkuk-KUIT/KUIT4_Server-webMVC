0bd9168edafd6aed95b0b65dd278add4fd3b154e
seungwoo1209 <2002msw@naver.com>
2024-10-30 21:19:27 +0900

    step 0 complete

---
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -0,0 +1,60 @@
+package core.jdbc;
+
+import jwp.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcTemplate<T> {
+
+    public void update(String sql, PreparedStatementSetter pstmtSetter) throws SQLException {
+
+        try (Connection conn = ConnectionManager.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmtSetter.setParameters(pstmt);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) throws SQLException {
+
+        ResultSet rs = null;
+        List<T> users = new ArrayList<>();
+
+        try (Connection conn = ConnectionManager.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);){
+
+            rs = pstmt.executeQuery(); // 파라미터를 세팅하기도 전에 쿼리가 실행되서 결과가 반환됨
+            while (rs.next()) {
+                T object = rowMapper.mapRow(rs);
+                users.add(object);
+            }
+        }
+
+        return users;
+    }
+
+    public T queryForObject(String sql, PreparedStatementSetter pstmtSetter, RowMapper<T> rowMapper) throws SQLException {
+
+        ResultSet rs = null;
+        T object = null;
+
+        try (Connection conn = ConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);){
+
+            pstmtSetter.setParameters(pstmt);
+
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                object = rowMapper.mapRow(rs);
+            }
+        }
+
+        return object;
+    }
+
+
+
+}
