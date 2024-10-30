abd2121ead92d60b1c66ce3a6cc38ca58782320f
yskim6772 <yskim6772@gmail.com>
2024-10-30 16:08:37 +0900

    [feat] : requirement step 1 - #3

---
+++ src/main/java/jwp/controller/ShowController.java
@@ -0,0 +1,26 @@
+package jwp.controller;
+
+import core.mvc.Controller;
+import jwp.dao.QuestionDao;
+import jwp.model.Question;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShowController implements Controller {
+
+    private final QuestionDao questionDao = new QuestionDao();
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+        String questionIdParam = req.getParameter("questionId");
+        int questionId = Integer.parseInt(questionIdParam);
+
+        Question question = questionDao.findByQuestionId(questionId);
+
+        req.setAttribute("question", question);
+
+        return "/qna/show.jsp";
+    }
+}
