338bd513cfd3c4624b973408a662c612633ddce9
hd0rable <kanghuijin12@gmail.com>
2024-11-05 16:27:08 +0900

    feat: 미션구현완료

--- src/main/java/core/mvc/view/JspView.java
+++ src/main/java/core/mvc/view/JspView.java
@@ -3,6 +3,7 @@ package core.mvc.view;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class JspView implements View {
 
@@ -15,12 +16,18 @@ public class JspView implements View {
     }
 
     @Override
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        for (Map.Entry<String, Object> entry : model.entrySet()) {
+            request.setAttribute(entry.getKey(), entry.getValue());
+        }
+
         if (viewName.startsWith(REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
-            return;
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher(viewName);
+            rd.forward(request, response);
         }
-        RequestDispatcher rd = request.getRequestDispatcher(viewName);
-        rd.forward(request, response);
     }
+
 }
--- src/main/java/jwp/controller/user/ListUserController.java
+++ src/main/java/jwp/controller/user/ListUserController.java
@@ -1,8 +1,7 @@
 package jwp.controller.user;
 
-import core.mvc.Controller;
-import core.mvc.view.JspView;
-import core.mvc.view.View;
+import core.mvc.AbstractController;
+import core.mvc.view.ModelAndView;
 import jwp.dao.UserDao;
 import jwp.util.UserSessionUtils;
 
@@ -10,17 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class ListUserController implements Controller {
+public class ListUserController extends AbstractController {
 
     private final UserDao userDao = new UserDao();
 
     @Override
-    public View execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         HttpSession session = req.getSession();
         if (UserSessionUtils.isLogined(session)) {
-            req.setAttribute("users", userDao.findAll());
-            return new JspView("/user/list.jsp");
+            return jspView("/user/list.jsp").addObject("users", userDao.findAll());
         }
-        return new JspView("redirect:/user/loginForm");
+        return jspView("redirect:/user/loginForm");
     }
 }
