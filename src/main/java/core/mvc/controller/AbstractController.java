514dde6d2f196410c9c31a01ca81f3c09e7c3117
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-11-06 19:01:45 +0900

    fix: 로그인 화면 오류 수정

--- src/main/java/core/mvc/controller/AbstractController.java
+++ src/main/java/core/mvc/controller/AbstractController.java
@@ -4,6 +4,7 @@ import core.mvc.view.JsonView;
 import core.mvc.view.JspView;
 import core.mvc.view.ModelAndView;
 
+// 하위호환성때문에 .. 기존의 Controller를 확장해서 구현 (라는 추측!?)
 public interface AbstractController extends Controller {
 
     default ModelAndView jspView(String viewName) {
