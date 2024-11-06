5159054be3cf96ee1148105263b4721fa088fa72
yskim6772 <yskim6772@gmail.com>
2024-11-06 13:55:31 +0900

    [feat] : 6th mission

--- src/main/java/core/mvc/view/View.java
+++ src/main/java/core/mvc/view/View.java
@@ -2,7 +2,9 @@ package core.mvc.view;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public interface View {
-    void render(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
+// 여기서 render의 인자로 model 관련을 넣어야 함 -> ModelAndView에서 view.render에서 현재 오류 발생
