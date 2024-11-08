2ced1d2078a3ead2012de4921b88c5f08eab220c
SangHyun Jung <wjdtkdgus876@naver.com>
2024-11-08 21:34:24 +0900

    6주차 미션완료

--- src/main/java/core/mvc/view/JspView.java
+++ src/main/java/core/mvc/view/JspView.java
@@ -14,7 +14,7 @@ public class JspView implements View {
         this.viewName = viewName;
     }
 
-    // JSP에 모델 데이터 전달 / 클라이언트로 페이지 응답
+    // JSP에 모델 데이터 전달 & 클라이언트로 페이지 응답
     @Override
     public void render(Map<String,Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         // "redirect:"라면 해당 URL로 리다이렉트
